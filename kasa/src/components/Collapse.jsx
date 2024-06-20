2d99338911870369dc48e7ac4bf77c4ee56d75a5
azorophelie <azor.ophelie@yahoo.fr>
2024-06-20 15:25:19 +0200

    création de la page à propos

---
+++ kasa/src/components/Collapse.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+
+
+const Collapse = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse-container">
+      <div className="collapse-title" onClick={toggleCollapse}>
+        <h3>{title}</h3>
+        <FontAwesomeIcon icon={isOpen ? faChevronUp : faChevronDown} className="arrow" />
+      </div>
+      <div className={`collapse-content ${isOpen ? 'open' : ''}`}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Collapse;
--- kasa/src/pages/About.js
+++ kasa/src/pages/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import image from '../images/Banner_a_propos.png';
 
 import Styles from '../SCSS/main.scss';
+import Collapse from '../components/Collapse';
 
 
 const About = () => (
@@ -15,6 +16,27 @@ const About = () => (
 
      
     </div>
+    <div>
+      <Collapse title="Fiabilité">
+        <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et 
+          toutes les informations sont regulièrement vérifiées pars nos équipes.</p>
+      </Collapse>
+      <Collapse title="Respect">
+        <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de 
+          perturbation du voisinage entraînera une exclusion de notre plateforme</p>
+      </Collapse>
+      <Collapse title="Service">
+        <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de 
+          perturbation du voisinage entraînera une exclusion de notre plateforme</p>
+      </Collapse>
+      <Collapse title="Sécurité">
+        <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+          correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+          locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+          également des ateliers sur la sécurité domestique pour nos hôtes. 
+        </p>
+      </Collapse>
+      </div>
     
    
   </div>
